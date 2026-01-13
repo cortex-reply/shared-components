5fc7b09a4ff8c8278973d5bbd54ead29515ab969
Rob Ellison <r.ellison@reply.com>
2026-01-13 15:29:25 +0000

    fix: types

--- packages/auth/src/payload-jwt/authenticateRequest.ts
+++ packages/auth/src/payload-jwt/authenticateRequest.ts
@@ -127,62 +127,61 @@ export async function authenticateRequest({ req, payload }: AuthenticateRequestO
     }
 }
 
-export async function authenticateRequestHeaders({ headers, payload }: { headers: Headers, payload?: Payload }) {
-    if (headers.get('authorization')) {
+export async function authenticateRequestHeaders({ headers, payload }: { headers: Headers, payload: Payload }) {
+    if (!headers.get('authorization')) {
+        throw createAuthError("No authorization header provided", 401);
+    }
 
-        const session = await verifyToken(headers.get('authorization')!.replace('Bearer ', ''));
+    const session = await verifyToken(headers.get('authorization')!.replace('Bearer ', ''));
 
-        if (!session || !session.sub || !session.extra) throw createAuthError("No valid session found", 401);
-        const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID!;
-        const permissions = ((session.resource_access as Record<string, { roles?: string[] }>)?.[OAUTH_CLIENT_ID]?.roles as string[] | undefined);
-        if (!permissions) throw createAuthError("User does not have permission to access this application.", 403);
+    if (!session || !session.sub || !session.extra) throw createAuthError("No valid session found", 401);
+    const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID!;
+    const permissions = ((session.resource_access as Record<string, { roles?: string[] }>)?.[OAUTH_CLIENT_ID]?.roles as string[] | undefined);
+    if (!permissions) throw createAuthError("User does not have permission to access this application.", 403);
 
-        if (!payload) throw createAuthError("Payload instance is required for Keycloak user normalisation", 500);
-        const payloadUser = (await payload.find({ collection: 'users', depth: 1, limit: 1, draft: false, overrideAccess: true, where: { email: { equals: session.extra.email } } })).docs[0]
+    if (!payload) throw createAuthError("Payload instance is required for Keycloak user normalisation", 500);
+    const payloadUser = (await payload.find({ collection: 'users', depth: 1, limit: 1, draft: false, overrideAccess: true, where: { email: { equals: session.extra.email } } })).docs[0]
 
-        if (!payloadUser && session.extra) {
+    if (!payloadUser && session.extra) {
+        // create the user in Payload
+        const newUser = await payload.create({
+            collection: 'users',
+            data: {
+                email: session.extra.email,
+                name: session.extra.name,
+                role: permissions[0] || 'user',
+                enabled: true,
+                accounts: [
+                    {
+                        provider: 'keycloak', providerAccountId: session.sub, type: 'oidc',
+                    }
+                ],
+            },
+            draft: false,
+            overrideAccess: true,
+        })
+        console.log("Created new Payload user for Keycloak user:", newUser.id, newUser.email)
+        return { method: 'bearer', ...newUser }
 
-
-            // create the user in Payload
-            const newUser = await payload.create({
+    } else if (payloadUser) {
+        // update user role if changed
+        if (payloadUser.role !== permissions[0]) {
+            await payload.update({
                 collection: 'users',
+                id: payloadUser.id,
                 data: {
-                    email: session.extra.email,
-                    name: session.extra.name,
                     role: permissions[0] || 'user',
-                    enabled: true,
-                    accounts: [
-                        {
-                            provider: 'keycloak', providerAccountId: session.sub, type: 'oidc',
-                        }
-                    ],
                 },
                 draft: false,
                 overrideAccess: true,
             })
-            console.log("Created new Payload user for Keycloak user:", newUser.id, newUser.email)
-            return { method: 'bearer', ...newUser }
-
-        } else if (payloadUser) {
-            // update user role if changed
-            if (payloadUser.role !== permissions[0]) {
-                await payload.update({
-                    collection: 'users',
-                    id: payloadUser.id,
-                    data: {
-                        role: permissions[0] || 'user',
-                    },
-                    draft: false,
-                    overrideAccess: true,
-                })
-                console.log(`Updated Payload user role for ${payloadUser.email} to ${permissions}`);
-            }
-
-            return {method: 'bearer', ...payloadUser }
-
-        } else {
-            throw createAuthError("No user found for the given session", 401);
+            console.log(`Updated Payload user role for ${payloadUser.email} to ${permissions}`);
         }
+
+        return {method: 'bearer', ...payloadUser }
+
+    } else {
+        throw createAuthError("No user found for the given session", 401);
     }
 }
 
